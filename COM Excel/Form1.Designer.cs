0b0da9d8e2ecbcc4bec6b9a626d2899fa63ddbae
Hlebniy <75940872+Hlebniy@users.noreply.github.com>
2023-06-20 20:59:41 +0300

    Update Form1.Designer.cs

--- COM Excel/Form1.Designer.cs
+++ COM Excel/Form1.Designer.cs
@@ -108,7 +108,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
-            this.Text = "ПЕНИН";
+            this.Text = "Form";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
